be386ae37f16510046eebfdc513b7878e7b8ff0d
SpanishJoplin <jpreynoldsku@gmail.com>
2018-03-25 22:46:56 -0600

    commit 2 - created game board

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+//need to assign a keydown function to each box
+//
+
+const playerOne = document.getElementById('red');
+const playerTwo = document.getElementById('blue');
+
+
+console.log(playerOne);
+console.log(playerTwo);
+
+let redCarPosition = 0;
+let blueCarPosition = 0;
+
+
