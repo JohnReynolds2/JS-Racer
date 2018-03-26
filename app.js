e3222c1f467a4602bb18aa1f7f07dcca0e0ce75f
SpanishJoplin <jpreynoldsku@gmail.com>
2018-03-25 23:48:11 -0600

    Commit 4 - Moves red square across page but no win, no blue square, movement, no reset

--- app.js
+++ app.js
@@ -23,14 +23,27 @@ playerOne.addEventListener('click', (event) => {
 
 
 //function that will communicate with the red square and move it right one place
+//this moves the red square across the page but with no winning alert
 const moveRed = () => {
+	document.getElementById('red').nextElementSibling.setAttribute('id', 'red');
+	document.querySelector('#red').removeAttribute('id');
+	lane1.push();
+	if(lane1.length >= 6) {
+		alert("red wins");
+	}
 	
 };
 
 //function that will tell blue square to move right one place
 const moveBlue = () => {
+	playerTwo.nextElementSibling.setAttribute('id', 'blue');
+	document.querySelector('blue').removeAttribute('id');
+	lane2.push('yo');
+	if(lane2.length >= 6) {
+		alert("blue wins");
+	}
 
-}
+};
 
 //found the keycodes for right-arrow and 'd' key online. need to someonehow create the function
 //since both squares are moving right you can combine both keys to the same function.
